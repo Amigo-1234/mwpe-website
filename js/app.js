02bba51d07786ed6377f6409cb0c258575310e79
Amigo <Olamidesamson841@gmail.com>
2026-05-18 18:02:22 -0600

    second chnage

--- js/app.js
+++ js/app.js
@@ -3,9 +3,17 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const closeMenuBtn = document.querySelector(".close-menu-btn");
 
 menuBtn.addEventListener("click", () => {
+
   mobileMenu.classList.add("active");
+
+  document.body.style.overflow = "hidden";
+
 });
 
 closeMenuBtn.addEventListener("click", () => {
+
   mobileMenu.classList.remove("active");
+
+  document.body.style.overflow = "auto";
+
 });
